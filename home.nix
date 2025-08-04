4954850ffea3d958e369fd20547aae32a10b6c36
clarencelubrin <culubrin@up.edu.ph>
2025-08-05 03:10:06 +0800

    Update on 2025-08-05 03:10:06

--- home.nix
+++ home.nix
@@ -102,6 +102,16 @@
       # };
     };
   };
+
+  firefox = {
+    name = "Firefox";
+    genericName = "Web Browser";
+    exec = "firefox %U";
+    terminal = false;
+    categories = [ "Application" "Network" "WebBrowser" ];
+    mimeType = [ "text/html" "text/xml" ];
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
