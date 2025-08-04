010dbe208167cdbc381a62b1f85ee7d48e058f6f
clarencelubrin <culubrin@up.edu.ph>
2025-08-05 02:13:44 +0800

    Update on 2025-08-05 02:13:44

--- configuration.nix
+++ configuration.nix
@@ -114,7 +114,7 @@ in
   xdg.portal = {
     enable = true;
     extraPortals = [
-      pkgs.xdg-desktop-portal-gnome
+    # pkgs.xdg-desktop-portal-gnome
       pkgs.xdg-desktop-portal-gtk
     ];
   };
