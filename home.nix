c6826ffc6038d4d839c14076196448c04bf00c58
clarencelubrin <culubrin@up.edu.ph>
2025-08-05 00:54:52 +0800

    Update on 2025-08-05 00:54:52

--- home.nix
+++ home.nix
@@ -71,9 +71,31 @@
   };
  
   # Enable GNOME Shell extensions
-  home.gnomeExtensions = with pkgs.gnome3; [
-    dashToDock
-  ];
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/shell" = {
+        # disable-user-extensions = true; # Optionally disable user extensions entirely
+        enabled-extensions = [
+          # Put UUIDs of extensions that you want to enable here.
+          # If the extension you want to enable is packaged in nixpkgs,
+          # you can easily get its UUID by accessing its extensionUuid
+          # field (look at the following example).
+          pkgs.gnomeExtensions.gsconnect.extensionUuid
+
+          # Alternatively, you can manually pass UUID as a string.
+          "blur-my-shell@aunetx"
+          # ...
+        ];
+      };
+
+      # Configure individual extensions
+      "org/gnome/shell/extensions/blur-my-shell" = {
+        brightness = 0.75;
+        noise-amount = 0;
+      };
+    };
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
