2cb5a8cf480e6051b2bea051ac94ee9d1b228272
clarencelubrin <culubrin@up.edu.ph>
2025-08-04 23:07:10 +0800

    Update on 2025-08-04 23:07:10

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,9 @@
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  # Nix settings
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
  
