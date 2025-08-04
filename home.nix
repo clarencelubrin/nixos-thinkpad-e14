557ba405ecd79f3759a785aea6397a699b07b7a7
clarencelubrin <culubrin@up.edu.ph>
2025-08-05 00:50:32 +0800

    Update on 2025-08-05 00:50:32

--- home.nix
+++ home.nix
@@ -5,8 +5,6 @@
   # manage.
   home.username = "lubrin";
   home.homeDirectory = "/home/lubrin";
-  # Allow unfree packages in Home Manager
-  home.allowUnfree = true;
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
