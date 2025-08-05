7aecad2be98f51a076d58cc340e98d9e3d3f1bc2
clarencelubrin <culubrin@up.edu.ph>
2025-08-05 10:25:37 +0800

    Update on 2025-08-05 10:25:37

--- configuration.nix
+++ configuration.nix
@@ -73,6 +73,21 @@ in
    ];
   };
   environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
+
+  # Fingerprint
+  systemd.services.fprintd = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "simple";
+  };
+
+  # Install the driver
+  services.fprintd.enable = true;
+  # If simply enabling fprintd is not enough, try enabling fprintd.tod...
+  services.fprintd.tod.enable = true;
+  # ...and use one of the next four drivers
+  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix-550a; # Goodix 550a driver (from Lenovo)
+
+
   # Set your time zone.
   time.timeZone = "Asia/Manila";
   
