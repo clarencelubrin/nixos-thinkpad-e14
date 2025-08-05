50a299005465ae6bbfb64a4a0149bd95ebbf8d8f
clarencelubrin <culubrin@up.edu.ph>
2025-08-05 20:29:03 +0800

    Update on 2025-08-05 20:29:03

--- home.nix
+++ home.nix
@@ -13,7 +13,7 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
-
+  nixpkgs.config.allowUnfree = true;
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
