14fa862bffba2f90f10d9fa4a96413aea9044965
clarencelubrin <culubrin@up.edu.ph>
2025-08-05 23:10:03 +0800

    changed flake

--- flake.nix
+++ flake.nix
@@ -15,14 +15,21 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     # use "nixos", or your hostname as the name of the configuration
     # it's a better practice than "default" shown in the video
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
+      # specialArgs = {inherit inputs;};
+      specialArgs = { inherits nix4nvchad; };
       modules = [
         ./configuration.nix
-        inputs.home-manager.nixosModules.default
+        # inputs.home-manager.nixosModules.default
+	home-manager.nixosModules.default
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.lubrin = import ./home.nix;
+        }
       ];
     };
   };
