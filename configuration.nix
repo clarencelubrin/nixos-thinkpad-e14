af3779b52f66493096e1ccd1bcbd351687159fa4
clarencelubrin <culubrin@up.edu.ph>
2025-08-05 22:18:19 +0800

    Update on 2025-08-05 22:18:19

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 let
   # Ensure inputs is passed from specialArgs
   inputs = config.specialArgs.inputs;
