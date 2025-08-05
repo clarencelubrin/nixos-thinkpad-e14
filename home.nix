69b63695fde1ea9dff57f4425c1e01f791f6e5dc
clarencelubrin <culubrin@up.edu.ph>
2025-08-05 11:46:00 +0800

    Update on 2025-08-05 11:46:00

--- home.nix
+++ home.nix
@@ -79,7 +79,7 @@
       "org/gnome/desktop/interface" = {
 	  color-scheme = "prefer-dark";
 	  icon-theme = "WhiteSur";
-	  cursor-theme = "bibata-cursors";
+	  cursor-theme = "Bibata-Modern-Classic";
       };
       "org/gnome/desktop/wm/preferences" = {
           button-layout = "appmenu:minimize,maximize,close";
