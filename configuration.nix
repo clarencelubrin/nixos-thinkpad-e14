0f2dd76f3807c80b72a721f88a8abd3e899d128e
clarencelubrin <culubrin@up.edu.ph>
2025-08-05 22:55:32 +0800

    Update on 2025-08-05 22:55:32

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,7 @@ in
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
+       inputs.nix4nvchad.homeManagerModule
     ];
 
   # Bootloader.
@@ -195,6 +196,23 @@ in
       })
     ];
   };
+  programs.nvchad = {
+    enable = true;
+    extraPackages = with pkgs; [
+      nodePackages.bash-language-server
+      docker-compose-language-service
+      dockerfile-language-server-nodejs
+      emmet-language-server
+      nixd
+      (python3.withPackages(ps: with ps; [
+        python-lsp-server
+        flake8
+      ]))
+    ];
+    hm-activation = true;
+    backup = true;
+  };
+
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
