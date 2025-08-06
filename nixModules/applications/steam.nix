96be87ab65c254c2ff7b754cc8f5ed77cac8ff80
clarencelubrin <culubrin@up.edu.ph>
2025-08-06 23:04:26 +0800

    Update on 2025-08-06 23:04:26

--- nixModules/applications/steam.nix
+++ nixModules/applications/steam.nix
@@ -1,9 +1,9 @@
 { pkgs, lib, config, ... }:
 {
   options = {
-    steam.enable = lib.mkEnableOption "enables steam";
+    steam-app.enable = lib.mkEnableOption "enables steam";
   };
-  config = lib.mkIf config.df-app.enable {
+  config = lib.mkIf config.steam-app.enable {
     programs.steam = {
       enable = true;
       extraCompatPackages = [ pkgs.proton-ge-bin ];
